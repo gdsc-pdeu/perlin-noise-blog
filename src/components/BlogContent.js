3eea3619be63a058063ca47cf9051e38e417b20f
Maharshi Pandya <maharshi.pandya2002@gmail.com>
2021-07-10 16:35:53 +0530

    :star: reached draw perlin console

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -126,7 +126,7 @@ function BlogContent() {
                         PerlinNoise.h
                     </span>
                     &nbsp;file copy and paste&nbsp;
-                    <a href="para-content https://github.com/Maharshi-Pandya/Perlin-Noise-Implementation/blob/master/cpp-test/pn_header/PerlinNoise.h">
+                    <a href="https://github.com/Maharshi-Pandya/Perlin-Noise-Implementation/blob/master/cpp-test/pn_header/PerlinNoise.h">
                         this code
                     </a>
                     &nbsp;from my Github repo. 
@@ -259,6 +259,122 @@ function BlogContent() {
                         Ctrl-C
                     </span>
                     &nbsp;, it exits "gracefully".
+                </p>
+
+                <p className="para-content mt-3">
+                    We also defined 2 global&nbsp;
+                    <span className="backtick-code">
+                        ints
+                    </span>
+                    &nbsp;(i.e.&nbsp;
+                    <span className="backtick-code">
+                        w_width
+                    </span>
+                    &nbsp;and&nbsp;
+                    <span className="backtick-code">
+                        w_height
+                    </span>
+                    ) which are the width and 
+                    the height of our grid, where visualisation takes place.
+                    <br/>
+                    In the next section, we will write a function&nbsp;
+                    <span className="backtick-code">
+                        SetConsoleSize()
+                    </span>
+                    &nbsp;which sets the width and the height of our grid 
+                    equal to the width and the height of our terminal, respectively.
+                </p>
+
+                <h1 className="pn-topic-header mt-5 liner">
+                    SetConsoleSize()
+                </h1>
+
+                <p className="para-content">
+                    Firstly, in your&nbsp;
+                    <span className="backtick-code">
+                        main.cpp
+                    </span>
+                    &nbsp;file add the following lines
+                </p>
+
+                <div className="codefield-container">
+                    <div className="codefield-cpp">
+                        <span className="cpp-comments">
+                            // Get the terminal size
+                        </span>
+                        <br/>
+                        <span className="cpp-dt">
+                            void
+                        </span>
+                        &nbsp;SetConsoleSize(
+                            <span className="cpp-dt">
+                                void
+                            </span>
+                        )
+                        <br />
+                        &#123;
+                        <br/>
+                        <span className="cpp-comments">
+                            // use the ioctl object
+                        </span>
+                        <br/>
+                        <span className="cpp-uddt">
+                            struct
+                        </span>
+                        &nbsp;winsize window;
+                        <br/>
+                        <span className="cpp-func">
+                            ioctl
+                        </span>
+                        (STDOUT_FILENO, TIOCGWINSZ, &window);
+                        
+                        <br/>
+                        <span className="cpp-comments">
+                            // set the world width and height (-2 for safety reasons)
+                        </span>
+                        <br/>
+                        w_width = (<span className="cpp-dt">int</span>)window.ws_col - <span className="cpp-consts">2</span>;
+                        <br/>
+                        w_height = (<span className="cpp-dt">int</span>)window.ws_row - <span className="cpp-consts">2</span>;
+                        <br/>
+                        &#125;
+                    </div>
+                    <div className="text-center mt-2 lan-id">
+                        Language: Cpp
+                    </div>
+                </div>
+
+                <p className="para-content mt-5">
+                    This might seem very alien to you, but in essence:
+                    <br/><br/>
+                    <i className="fa fa-caret-right" aria-hidden="true"></i>
+                    <span className="ml-3">
+                        In Linux,&nbsp;
+                        <span className="backtick-code">
+                            ioctl
+                        </span>
+                        &nbsp;is used to perform I/O control operation 
+                        which uses the ioctl object&nbsp;
+                        <span className="backtick-code">
+                            winsize
+                        </span>
+                        &nbsp;to get the window size of output buffer 
+                        (
+                            <span className="backtick-code">
+                                TIOCGWINSZ
+                            </span>
+                            &nbsp;is an integer which tells 
+                        the ioctl function to get the window size).
+                    </span>
+                    <br/>
+                    <i className="fa fa-caret-right" aria-hidden="true"></i>
+                    <span className="ml-3">
+                        Lastly, it sets the width and height variables, 
+                        once it gets the information about the terminal.
+                    </span>
+                    <br/><br/>
+                    This was all the setting up needed. Next section, 
+                    we will focus on the main visualisation. Suit yourself!
                 </p>
             </div>
         </Fragment>
